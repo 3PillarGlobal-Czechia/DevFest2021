73e200b67db4b4161e6dfc3ddc829cf203c3d650
Ondřej Pektor <ondrej.pektor@3pillarglobal.com>
2021-11-24 16:26:22 +0100

    All planning workshop live changes added

---
+++ 03-Blazor/BeerQuiz/BeerQuiz/Client/Model/QuizItemResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace BeerQuiz.Client.Model
+{
+    public class QuizItemResponse
+    {
+        public string Question { get; set; } = string.Empty;
+        public IEnumerable<string> Options { get; set; } = Enumerable.Empty<string>();
+        public int CorrectAnswerIndex { get; set; }
+        public string GetAnswerString() => Options.ElementAt(CorrectAnswerIndex);
+    }
+}
